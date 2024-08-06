1f30af8ea92b1a0b48222316ce4819853dc4ec28
Ilgiz Saburov <barysaburov@gmail.com>
2024-08-06 22:57:04 +0300

    add cod

---
+++ index.js
@@ -0,0 +1,5 @@
+const daysOfWeek = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+const toDay = new Date();
+const dayOfWeek = today.getDay();
+
+alert(dayOfWeek[dayOfWeek]);
